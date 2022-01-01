303d22b4a71b140dfdbe515cb9d89d0336c15472
forcewind <forcewind@vip.qq.com>
2022-01-02 00:36:36 +0800

    fix: navbar index

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -96,7 +96,7 @@ const MenuItem = (menu: MenuItem) => (
   position: fixed;
   top: 0;
   background: rgba(0, 0, 0, 0.1);
-  z-index: 1;
+  z-index: 100;
   border-bottom: 1px solid #fff;
 }
 .inner {
